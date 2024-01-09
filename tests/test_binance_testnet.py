93101781dc89ce3ffbd4b30a73449a9f91830bc9
Brian of London (home imac) <brian@v4v.app>
2024-01-09 15:57:17 +0200

    Deal with situation when Binance testnet isn't working Fixes #10
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- tests/test_binance_testnet.py
+++ tests/test_binance_testnet.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 
 import pytest
+import requests  # type: ignore
 
 from v4vapp_binance.binance import (
     BinanceErrorLowBalance,
@@ -11,7 +12,16 @@ from v4vapp_binance.binance import (
 )
 
 
-def test_get_current_price_testnet():
+# This fixture will return True if the Binance Testnet is down and False otherwise
+@pytest.fixture(scope="module")
+def binance_testnet_down():
+    response = requests.get("https://testnet.binance.vision/api/v3/ping")
+    return response.status_code != 200
+
+
+def test_get_current_price_testnet(binance_testnet_down):
+    if binance_testnet_down:
+        pytest.skip("Binance Testnet is down, skipping test")
     price = get_current_price("BTCUSDT", testnet=True)
     assert price is not None
     assert type(price) is dict
@@ -23,7 +33,9 @@ def test_get_current_price_testnet():
     assert float(price["current_price"]) > 0.0
 
 
-def test_get_balances_testnet():
+def test_get_balances_testnet(binance_testnet_down):
+    if binance_testnet_down:
+        pytest.skip("Binance Testnet is down, skipping test")
     balances = get_balances(["BTC", "HIVE"], testnet=True)
     assert balances is not None
     assert type(balances) is dict
@@ -33,7 +45,9 @@ def test_get_balances_testnet():
         assert balances["SATS"] >= 0.0
 
 
-def test_place_order_testnet():
+def test_place_order_testnet(binance_testnet_down):
+    if binance_testnet_down:
+        pytest.skip("Binance Testnet is down, skipping test")
     price = get_current_price("HIVEBTC", testnet=True)
     balances = get_balances(["BTC", "HIVE"], testnet=True)
     if "BTC" in balances:
@@ -64,7 +78,9 @@ def test_place_order_testnet():
 
 
 @pytest.mark.parametrize("side", ["BUY", "SELL"])
-def test_place_order_now_testnet(side):
+def test_place_order_now_testnet(side, binance_testnet_down):
+    if binance_testnet_down:
+        pytest.skip("Binance Testnet is down, skipping test")
     ans = place_order_now(
         from_asset="HIVE",
         to_asset="BTC",
@@ -85,7 +101,9 @@ def test_place_order_now_testnet(side):
 
 
 @pytest.mark.parametrize("side", ["BUY", "SELL"])
-def test_place_order_now_minimums_testnet(side):
+def test_place_order_now_minimums_testnet(side, binance_testnet_down):
+    if binance_testnet_down:
+        pytest.skip("Binance Testnet is down, skipping test")
     ans = place_order_now(
         from_asset="HIVE",
         to_asset="BTC",
@@ -118,7 +136,9 @@ def test_place_order_now_minimums_testnet(side):
     assert ans.get("balances").get("delta").get("BTC") is not None
 
 
-def test_balance_error_testnet():
+def test_balance_error_testnet(binance_testnet_down):
+    if binance_testnet_down:
+        pytest.skip("Binance Testnet is down, skipping test")
     balance = get_balances(["BTC", "HIVE"], testnet=True)
 
     # Check that place_order_now raises a specific error
